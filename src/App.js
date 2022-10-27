0619f07e4db5b5cacd328d587229bd3882b96e4e
DAGERON <den-kir1@yandex.ru>
2022-10-27 13:54:05 +0300

    add: file system

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
-import './styles/App.css'
-import './styles/Navbar.css'
+import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar">
-          <button className='nav-btn-roll-up'/>
-          <button className='nav-btn-full-screen'/>
-          <button className='nav-btn-close'/>
-      </nav>
+        <div className="container">
+            <header className="header-window">
+                <nav className="navbar">
+                    <button className='nav-btn-roll-up'/>
+                    <button className='nav-btn-full-screen'/>
+                    <button className='nav-btn-close'/>
+                </nav>
+            </header>
+            <main className="main">
+                <div className="left-bufferLine">
+
+                </div>
+                <div className="main-container">
+                    <div className="up-bufferLine">
+
+                    </div>
+                    <div className="work-flow">
+                        <aside className="list-users">
+
+                        </aside>
+                        <div className="document-flow">
+                            <div className="bottom-document">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+
     </div>
   );
 }
