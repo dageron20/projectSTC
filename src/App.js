9846a3d7774bbe911bc47bbe7726d8762f6d1284
Artem <artem_6091@mail.ru>
2022-10-27 20:59:50 +0300

    add: BottomDocument

--- src/App.js
+++ src/App.js
@@ -26,9 +26,31 @@ function App() {
                         </aside>
                         <div className="document-flow">
                             <div className="bottom-document">
-
+                                <div className="left-menu">
+                                    <button className='btn-document'>Документы</button>
+                                </div>
+                                <div className="right-menu">
+                                    <p>Колличесвто отправок</p>
+                                    <div className='box-spinner'>
+                                        <input className='in-num' type='number' min='1' max='99' step='1' value='1'/>
+                                        <span className="span-strip"></span>
+                                        <span className="span-minus"></span>
+                                        <span className="span-plus"></span>
+                                    </div>
+                                    {/*<button disabled className='btn-send'>Отправить</button>*/}
+                                    <button className='btn-send-true'>
+                                         <span>
+                                            Отправить
+                                         </span>
+                                        <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 14L14.8371 7L0 0V5.44444L10.598 7L0 8.55556V14Z" fill="#FCFCFD"/>
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                         </div>
+
+
                     </div>
                 </div>
             </main>
