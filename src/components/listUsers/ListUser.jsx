c37730c6ad0d10612a48173b056fc34ec3f6fd58
dageron20 <den-kir1@yandex.ru>
2022-11-16 13:19:28 +0300

    fix conflict

--- src/components/listUsers/ListUser.jsx
+++ src/components/listUsers/ListUser.jsx
@@ -2,36 +2,35 @@ import React, {useRef, useState} from "react";
 import ItemUser from "../itemUser/ItemUser";
 import { Buffer } from 'buffer';
 
-const ListUser = ({clients}) => {
-    const [file, setFile] = useState();
-    const [fileurl, setFileurl] = useState();
-    const [jsonfile, setJsonfile] = useState();
-    const fileReader = new FileReader();
-    fileReader.onloadend = () => {
-        setFileurl(fileReader.result);
-        const obj = JSON.parse(JSON.stringify(fileReader.result));
-        const json = Buffer.from(obj.substring(29), "base64").toString();
-        const result = JSON.parse(json);
-        console.log(result);
-        setJsonfile(result);
-    };
-
-    fileReader.addEventListener('progress', (event) => { /* Процент загрузки json файла в console */
-        if (event.loaded && event.total) {
-            const percent = (event.loaded / event.total) * 100;
-            console.log(`Progress: ${Math.round(percent)}`);
-        }
-    });
-
-    const handleOnChange = (event) => {
-        event.preventDefault();
-        const clients2 = event.target.files[0];
-        setFile(clients2);
-        fileReader.readAsDataURL(clients2);
-    }
-
-
     const ListUser = ({clients, state, setState}) => {
+
+        const [file, setFile] = useState();
+        const [fileurl, setFileurl] = useState();
+        const [jsonfile, setJsonfile] = useState();
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+            setFileurl(fileReader.result);
+            const obj = JSON.parse(JSON.stringify(fileReader.result));
+            const json = Buffer.from(obj.substring(29), "base64").toString();
+            const result = JSON.parse(json);
+            console.log(result);
+            setJsonfile(result);
+        };
+
+        fileReader.addEventListener('progress', (event) => { /* Процент загрузки json файла в console */
+            if (event.loaded && event.total) {
+                const percent = (event.loaded / event.total) * 100;
+                console.log(`Progress: ${Math.round(percent)}`);
+            }
+        });
+
+        const handleOnChange = (event) => {
+            event.preventDefault();
+            const clients2 = event.target.files[0];
+            setFile(clients2);
+            fileReader.readAsDataURL(clients2);
+        }
+
         return (
             <aside className="list-users">
                 <div className="all-users">
@@ -46,7 +45,7 @@ const ListUser = ({clients}) => {
                            onChange={handleOnChange}
                            accept=".json"/>
                 </form>
-                {clients.map(clients => <ItemUser {...clients} key={clients.ip} clients={clients}/>)}
+
                 {state ?
                     <div className="list-users-settings">
                         <div className="list-users-settings-window">
@@ -116,7 +115,6 @@ const ListUser = ({clients}) => {
                 </div>
             </aside>
         )
-    }
 }
 
 export default ListUser;
